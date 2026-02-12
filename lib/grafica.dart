9266d7822e51f3f2f7d167a3f6e08ee8900b452a
Fidi <fidelgmtnez1@gmail.com>
2026-02-12 14:13:58 -0600

    pantalla de gráfica 2

--- lib/grafica.dart
+++ lib/grafica.dart
@@ -1,4 +1,5 @@
-//import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'dart:html';
 import 'package:flutter/rendering.dart';
 
@@ -7,6 +8,7 @@ void main(){
 //Tamaño gráfica
 CanvasElement canvas = new CanvasElement(height: 300, width: 200);
 document.body.append(canvas);
+
 
 
 };
